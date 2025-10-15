748774062ef886b29e7aa58a2ee238077df631ed
Koplin <koplin@gmail.com>
2025-10-15 15:18:55 -0300

    .

--- src/constants/parceiro-constants.ts
+++
@@ -1,9 +0,0 @@
-export const TIPO_PESSOA_OPTIONS = [
-  { value: "PF", label: "Pessoa Física" },
-  { value: "PJ", label: "Pessoa Jurídica" },
-];
-
-export const CONTA_TIPO_OPTIONS = [
-  { value: 1, label: "Conta Corrente" },
-  { value: 2, label: "Conta Poupança" },
-];
